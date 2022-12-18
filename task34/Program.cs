9ff9f4ee7255aea898436be88b373a01fafb931c
Dmitry2707 <kemper1495@gmail.com>
2022-12-18 15:03:25 +0300

    task34

---
+++ task34/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+//Диапазон трехзначных чисел, ввод случайный.
+int[] array = new int [4];
+Random random = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(100, 1000);
+}
+Console.WriteLine(string.Join(",", array));
+Console.WriteLine("Количество четных числел в массиве равнo: {0}", newcount(array));
+int newcount(int[] array)
+{
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+        count++;
+}
+return count;
+}
---
+++ task36/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+//Диапазон любой, ввод зависит от пользователя. (размер, значения)
+int[] array = new int [4];
+Random random = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(1, 9);
+}
+Console.WriteLine(string.Join(",", array));
+Console.WriteLine("Сумма элементов, стоящих на нечетных позициях равна: {0}", Sumoddposition(array));
+int Sumoddposition(int[] array)
+{
+int Sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if ((array.Length & 2) != 0)
+        Sum += array[i];
+}
+return Sum;
+}
